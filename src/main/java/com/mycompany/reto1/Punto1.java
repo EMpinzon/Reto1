5030a661c023b97879049414b6ecee0f616b9375
EMpinzon <EdgarYMauricio@outlook.es>
2023-12-11 12:15:35 -0500

    Añadiendo dos puntos, eliminando clase sobrante

--- src/main/java/com/mycompany/reto1/Punto1.java
+++ src/main/java/com/mycompany/reto1/Punto1.java
@@ -4,6 +4,8 @@
  */
 package com.mycompany.reto1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author user
@@ -14,7 +16,32 @@ public class Punto1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingresa el  numero hasta el cual se debe hacer el calculo" );
+        int sumaTotal = 0;
+        
+        int N = scanner.nextInt();
+        
+        for(int i = 1; i < N ; i++){
+        
+            if ( (i % 3 == 0 ) && (i % 5 == 0))  {
+                continue;
+            }
+            else if ( (i % 3) == 0  ){
+                
+                sumaTotal = sumaTotal + i;
+            
+            }else if ( (i % 5) == 0  ){
+            
+                sumaTotal = sumaTotal + i;
+            }
+        
+        }
+        
+        System.out.println("la suma total es : "+ sumaTotal);
+        
+        
+        
     }
     
 }
--- src/main/java/com/mycompany/reto1/Punto2.java
+++ src/main/java/com/mycompany/reto1/Punto2.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package com.mycompany.reto1;
-
+import java.util.Scanner;
 /**
  *
  * @author user
@@ -15,6 +15,81 @@ public class Punto2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingresa la contraseña a validar");
+        
+        String password = scanner.nextLine();
+        
+        boolean var1 = false;
+        boolean var2 = false;
+        boolean var3 = false;
+        boolean var4 = false; 
+        
+        if ( password.length() >= 8){
+        
+            for(int i = 0; i < password.length() ; i ++ ){
+                if (  password.charAt(i) == 'a' || password.charAt(i) == 'b' ||
+                password.charAt(i) == 'c' || password.charAt(i) == 'd' ||
+                 password.charAt(i) == 'd' || password.charAt(i) == 'e' ||       
+                password.charAt(i) == 'f' || password.charAt(i) == 'g' ||
+                password.charAt(i) == 'h' || password.charAt(i) == 'i' || 
+                password.charAt(i) == 'j' || password.charAt(i) == 'k' ||        
+                password.charAt(i) == 'l' || password.charAt(i) == 'm' ||  
+                password.charAt(i) == 'n' || password.charAt(i) == 'ñ' ||    
+                password.charAt(i) == 'o' || password.charAt(i) == 'p' ||   
+                password.charAt(i) == 'k' || password.charAt(i) == 'r' ||    
+                password.charAt(i) == 's' || password.charAt(i) == 't' ||   
+                 password.charAt(i) == 'u' || password.charAt(i) == 'v' ||  
+                password.charAt(i) == 'w' || password.charAt(i) == 'x' ||
+                password.charAt(i) == 'y' || password.charAt(i) == 'z'        ){
+                
+                var1 = true;
+                }
+                
+                if (  password.charAt(i) == 'A' || password.charAt(i) == 'B' ||
+                password.charAt(i) == 'C' || password.charAt(i) == 'D' ||
+                 password.charAt(i) == 'D' || password.charAt(i) == 'E' ||       
+                password.charAt(i) == 'F' || password.charAt(i) == 'G' ||
+                password.charAt(i) == 'H' || password.charAt(i) == 'I' || 
+                password.charAt(i) == 'J' || password.charAt(i) == 'K' ||        
+                password.charAt(i) == 'L' || password.charAt(i) == 'M' ||  
+                password.charAt(i) == 'N' || password.charAt(i) == 'Ñ' ||    
+                password.charAt(i) == 'O' || password.charAt(i) == 'P' ||   
+                password.charAt(i) == 'K' || password.charAt(i) == 'R' ||    
+                password.charAt(i) == 'S' || password.charAt(i) == 'T'|   
+                 password.charAt(i) == 'U' || password.charAt(i) == 'V' ||  
+                password.charAt(i) == 'W' || password.charAt(i) == 'X' ||
+                password.charAt(i) == 'Y' || password.charAt(i) == 'Z'        ){
+                
+                var2 = true;
+                }
+                
+                if (  password.charAt(i) == '1' || password.charAt(i) == '2' ||
+                password.charAt(i) == '3' || password.charAt(i) == '4' ||
+                 password.charAt(i) == '5' || password.charAt(i) == '6' ||       
+                password.charAt(i) == '7' || password.charAt(i) == '8' ||
+                password.charAt(i) == '9' || password.charAt(i) == '0'         ){
+                
+                var3 = true;
+                }
+                
+                if (  password.charAt(i) == '!' || password.charAt(i) == '@' ||
+                password.charAt(i) == '#' || password.charAt(i) == '$' ||
+                 password.charAt(i) == '%' || password.charAt(i) == '^' ||
+                password.charAt(i) == '&' || password.charAt(i) == '*'         ){
+                
+                var4 = true;
+                }
+                
+            }
+        }
+        
+        if( var1 == true && var2 == true && var3 == true && var4 == true  ){
+            System.out.println("Tu contraseña si cumple con lo requerido");
+        }else{
+            System.out.println("Tu contraseña no cumple con lo requerido");
+        }
     }
     
 }
