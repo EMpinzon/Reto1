36077645659ef9321d731c38c47b6196fd715e06
EMpinzon <EdgarYMauricio@outlook.es>
2023-12-11 10:40:54 -0500

    inicio de el proyecto

---
+++ src/main/java/com/mycompany/reto1/Reto1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.reto1;
+
+/**
+ *
+ * @author user
+ */
+public class Reto1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
