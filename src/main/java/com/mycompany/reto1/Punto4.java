894875b1fdf599f1fc189211c425e1c12ea979fa
EMpinzon <EdgarYMauricio@outlook.es>
2023-12-11 13:56:35 -0500

    añadir cuarto punto

--- src/main/java/com/mycompany/reto1/Punto4.java
+++ src/main/java/com/mycompany/reto1/Punto4.java
@@ -15,9 +15,22 @@ public class Punto4 {
      */
     public static void main(String[] args) {
         
+        int a = 0;
+        int b = 1;
         
+        for(int i = 0 ; i< 10 ; i++){
+            
+            System.out.print(a);
+            System.out.print(" ");
+            a = b;
+            b = a + b;
+        }
         
         
     }
     
+    
+    // con este programa generamos la variable a y la variable b, que seran utilizadas en un cilo for, que se repite diez veces, desde cero hasta 9.
+    // Por cada iteracion se imprime el valor de a,se deja un espacio a la derecha, y luego añadimos dentro de a el valor de b, y dentro de b el valor de a + b 
+    // Esta genera el efecto de que a se actualiza cada iteracion, con el valor de b , que es el doble de a, y b tambien se actualiza para tener el b anterior mas el nuevo a, que es como tener a a duplicado, pues el nuevo a es el mismo b anterior
 }
